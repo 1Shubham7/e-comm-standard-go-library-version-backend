171810f2010c702a95d66df8ed5b4c158bfaa18a
Shubham Singh <shubhammahar1306@gmail.com>
2023-09-03 11:27:56 +0530

    change + add: added the getmethod and moved code from serveHTTP to getProducts in handlers/products. works fine

--- handlers/products.go
+++ handlers/products.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"log"
 	"net/http"
-	"encoding/json"
 	"github.com/1shubham7/e-comm/data"
 )
 
@@ -16,18 +15,35 @@ func NewProducts(l*log.Logger) *Products {
 }
 
 // anything adds to the handler interface  needs to have a serve http method
-func (p *Products) ServeHTTP(rw http.ResponseWriter, h *http.Request) {
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 // we will now use encoding/json package to convert the struct we ahve into a json representative
-	listOfProducts := data.GetProducts()
+	// listOfProducts := data.GetProducts()
 	// now listOfProducts contains the list of products, but how do we return that to the user
 	// we do that by converting listOfProducts into json. and this the the way
 
 	// d, err := json.Marshal(listOfProducts)
 	// instead of Marshal we are using this better alternative
 
-	err := listOfProducts.ToJSON(rw) //ToJSON is in data/products
-	if err != nil {
-		http.Error(rw, "unable to convert data to json", http.StatusInternalServerError)
-	}
+	// err := listOfProducts.ToJSON(rw) //ToJSON is in data/products
+	// if err != nil {
+		// http.Error(rw, "unable to convert data to json", http.StatusInternalServerError)
+	// }
 	// rw.Write(d) instead of Marshal we are using this better alternative
+
+
+
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts (rw http.ResponseWriter, r *http.Request) {
+	listOfProducts := data.GetProducts()
+	err := listOfProducts.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to convert data to JSON", http.StatusInternalServerError)
+	}
 }
--- notes.md
+++ notes.md
@@ -4,3 +4,8 @@ This will give you output in nice JSON format
 
 <hr>
 
+## `curl localhost:6000 -XDELETE -v`
+
+This is give a DELETE request to server
+
+<hr>
